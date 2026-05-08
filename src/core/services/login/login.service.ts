f6a5ae4952f5419fe339d708d014f77a7c197249
alainkatayi <alainkatayi01@gmail.com>
2026-05-08 12:33:24 +0100

    feat(login-service): Ajout des méthodes pour recuperer le token, supprimer le toke et recuper le user

--- src/core/services/login/login.service.ts
+++ src/core/services/login/login.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environnemnt/env';
 import { LoginData } from '../../models/loginData';
@@ -19,5 +19,27 @@ export class LoginService {
 
   login(data:LoginData): Observable<LoginResponse>{
     return this.http.post<LoginResponse>(`${this.apiUrl}token/`,data)
+  }
+
+  get_user_local():LoginResponse | null {
+    if(typeof localStorage === 'undefined'){
+      return null
+    }
+    const data_user = localStorage.getItem(SESSION_KEY)
+    return data_user ? JSON.parse(data_user) as LoginResponse : null
+  }
+
+  getAuthHeaders():HttpHeaders{
+    const user = this.get_user_local()
+
+    return new HttpHeaders({
+      Accept :'application/json',
+      Authorization : `Bearer ${user?.access}`
+    })
+  }
+
+  logOut(){
+    localStorage.clear()
+
   }
 }
