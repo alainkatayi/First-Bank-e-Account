7867d23ad9b2e73d8b05745ee86c11a09290dd2d
alainkatayi <alainkatayi01@gmail.com>
2026-05-08 14:20:43 +0100

    feat(admin):Ajout de l'interface admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-        {
+    {
         path: 'login',
         title: 'Login',
         loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
     },
-        {
+    {
         path: 'request',
         title: 'Request',
         loadComponent: () => import('./pages/requests-list/requests-list.component').then(m => m.RequestsListComponent)
+    },
+    {
+        path: 'admin/layout',
+        title: 'Layout',
+        loadComponent: () => import('./pages/admin-layout/admin-layout.component').then(m => m.AdminLayoutComponent)
     },
 ];
