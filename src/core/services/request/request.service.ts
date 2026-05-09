4ea6b50c9f308b32f0e2e43d873dc59596143fb5
alainkatayi <alainkatayi01@gmail.com>
2026-05-09 23:53:49 +0100

    feat(request): implémentation du service loadRequestById

--- src/core/services/request/request.service.ts
+++ src/core/services/request/request.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { LoginService } from '../login/login.service';
 import { Observable } from 'rxjs';
 import { RequestPagination } from '../../models/pagination';
+import { Request } from '../../models/request';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,9 @@ export class RequestService {
       .set('page', page.toString())
       .set('page_size', page_size.toString())
     return this.http.get<RequestPagination>(`${this.apiUrl}request/index`, {params, headers})
+  }
+  loadRequestById(id:number):Observable<Request>{
+    const headers = this.stock_user_service.getAuthHeaders()
+    return this.http.get<Request>(`${this.apiUrl}request/${id}`, {headers})
   }
 }
